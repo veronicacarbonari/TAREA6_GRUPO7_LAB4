929b30791c09cdba35dbf5206c857ff21758321c
SebaGines <sebastian.gines@gmail.com>
2023-05-18 20:46:38 -0300

    Adding PersonaDaoImpl methods

--- src/daolmpl/PersonaDaoImpl.java
+++ src/daolmpl/PersonaDaoImpl.java
@@ -1,5 +1,132 @@
 package daolmpl;
 
-public class PersonaDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.PersonaDao;
+import entidad.Persona;
+
+public class PersonaDaoImpl implements PersonaDao  {
+
+	private static final String insert  = "INSERT INTO personas(Dni, Nombre, Apellido) VALUES(?, ?, ?)";
+	private static final String delete  = "DELETE FROM personas WHERE Dni = ?";
+	private static final String readall = "SELECT * FROM personas";
+	private static final String update  = "UPDATE personas SET Nombre = ? , Apellido = ? WHERE Dni = ?";
+	
+	@Override
+	public boolean insert(Persona persona) {
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean isOk = false;
+		try
+		{
+			statement = conexion.prepareStatement(insert);
+			statement.setString(1, persona.getDni());
+			statement.setString(2, persona.getNombre());
+			statement.setString(3, persona.getApellido());
+			if(statement.executeUpdate() > 0)
+			{
+				conexion.commit();
+				isOk = true;
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			try {
+				conexion.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		return isOk;
+	}
+
+	@Override
+	public boolean delete(Persona persona) {
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean isOk = false;
+		try 
+		{
+			statement = conexion.prepareStatement(delete);
+			statement.setString(1, persona.getDni());
+			if(statement.executeUpdate() > 0)
+			{
+				conexion.commit();
+				isOk = true;
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return isOk;
+	}
+
+	@Override
+	public boolean update(Persona persona) {
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean isOk = false;
+		try
+		{
+			statement = conexion.prepareStatement(update);
+			statement.setString(1, persona.getNombre());
+			statement.setString(2, persona.getApellido());
+			statement.setString(3, persona.getDni());
+			if(statement.executeUpdate() > 0)
+			{
+				conexion.commit();
+				isOk = true;
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			try {
+				conexion.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		return isOk;
+	}
+
+	@Override
+	public List<Persona> readAll() {
+		PreparedStatement statement;
+		ResultSet resultSet; 
+		ArrayList<Persona> personas = new ArrayList<Persona>();
+		Conexion conexion = Conexion.getConexion();
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement(readall);
+			resultSet = statement.executeQuery();
+			while(resultSet.next())
+			{
+				personas.add(getPersona(resultSet));
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return personas;
+	}
+	
+	private Persona getPersona(ResultSet resultSet) throws SQLException
+	{
+		String dni = resultSet.getString("Dni");
+		String nombre = resultSet.getString("Nombre");
+		String tel = resultSet.getString("Telefono");
+		return new Persona(dni, nombre, tel);
+	}
 
 }
